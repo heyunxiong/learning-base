d3054167edc65eef359cc4ea6969d605e9e782e2
He Yunxiong <hyxsean@gmail.com>
2021-12-12 12:44:19 +0800

    json test different encoding

--- JavaRoad/src/main/java/hyx2021/com/sean/json/JsonTest.java
+++ JavaRoad/src/main/java/hyx2021/com/sean/json/JsonTest.java
@@ -1,7 +1,8 @@
 package hyx2021.com.sean.json;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 /**
  * @author Yunxiong
@@ -9,9 +10,9 @@ import org.json.JSONObject;
  * @date 12/3/2021
  */
 public class JsonTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
 
-        String json = "{\"username\": \"sean\",\"id\": \"123\"}";
+     /*   String json = "{\"username\": \"sean\",\"id\": \"123\"}";
         JSONObject  jsonObject = null;
         try {
             jsonObject = new JSONObject(json);
@@ -23,7 +24,23 @@ public class JsonTest {
         }
         String username = jsonObject.getString("username");
         System.out.println(username);
-        System.out.println(jsonObject);
+        System.out.println(jsonObject);*/
+        System.out.println(System.getProperty("file.encoding"));
+        String s = "test: 中国 \"Account\"";
+        byte[] bytes = s.getBytes();
+        byte[] bytes1 = s.getBytes("UTF-8");
+        byte[] bytes2 = s.getBytes("ISO-8859-1");
+        byte[] gbks = s.getBytes("GBK");
+
+        System.out.println(Arrays.toString(bytes));
+        System.out.println(Arrays.toString(bytes1));
+        System.out.println(Arrays.toString(bytes2));
+        System.out.println(Arrays.toString(gbks));
+
+
+
+        System.out.println(new String(bytes, "GBK"));
+        System.out.println(Charset.defaultCharset().name());
 
     }
 }
